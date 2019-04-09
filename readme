0a6a628c5f25967063ac101313876080024f9bb9
Edward Salkield <edward.salkield@gmail.com>
2019-04-09 23:19:21 +0100

    Release 0.0.1
    Admin controls
    Readme
    Licensing
    Security and bug fixes

---
+++ readme
@@ -0,0 +1,84 @@
+# gremlins-photobot
+gremlins-photobot is a free as in freedom server systen for collating photos with friends, built with flask.
+Dedicated to my dad for his 51st birthday.
+
+
+## Getting Started
+### Prerequisites
+Python module dependencies:
+> flask
+> datetime
+> werkzeug
+> exif
+> json
+> configparser
+> urllib
+
+
+### Setup
+1. Clone the repository
+> $ git clone https://github.com/EdwardSalkield/gremlins-photobot.git
+> $ cd gremlins-photobot
+
+2. Configure the server
+  a. Setting up the config file
+_server.conf_ is the default configuration file. Within it, you can configure attributes such as the server hostname, port to bind to, and location of the photos directory (see next section).
+
+To location of the config file can be configured with the environment variable _GREMLINS\_PHOTOBOT\_SETTINGS:
+> export GREMLINS\_PHOTOBOT\_SETTINGS=/path/to/your/server.conf
+
+  b. Configuration
+gremlins-photobot
+
+Config Option | Meaning
+------------- | -------
+SERVER_NAME | Hostname and port to serve on. 0.0.0.0 for all interfaces.
+DEBUG | True puts the server into [flask debug mode] (http://flask.pocoo.org/docs/1.0/quickstart/#debug-mode).
+STATIC_LOCATION | The location of the directory to serve static files from.
+PHOTO_LOCATION | The subdirectory of STATIC_LOCATION to store albums.
+USER_FILE_LOCATION | The name of the user config file.
+ALLOWED_EXTENSIONS | The allowed filetypes for upload.
+DATE_FORMAT | The date format to be displayed.
+
+For more details, see the [flask builtin configuration values](http://flask.pocoo.org/docs/0.12/config/#builtin-configuration-values).
+
+
+3. Set up user accounts.
+User accounts are configured by USER_FILE_LOCATION. This file contains the username, the unique login token, and a boolean flag for admin access, per user.
+You should add at least one user to this file.
+
+Example users.json:
+>{
+>        "edd": {
+>                "token": "test",
+>                "admin": true
+>        }
+>}
+
+
+4. Launch the server
+Recommended: deploy the flask server as per [these instructions](http://flask.pocoo.org/docs/0.12/deploying/)
+Alternative: Run Flask's built-in server:
+> python3 server.py
+
+
+5. Connecting to the server
+Users login to the server with url-encoded tokens. In a web browser, navigate to http://yourdomain.tld:port/login?name=username&token=yourtoken.
+
+
+6. Administrator access
+Users with the admin flag set to true in USER_FILE_LOCATION are permitted to access the /admin url.
+From here, the server cache can be reset.
+
+
+## Built with
+* [flask](https://github.com/pallets/flask)
+* [jinja2](https://github.com/pallets/jinja)
+
+
+## Authors
+* **Edd Salkield**
+
+
+## License
+This project is licensed under GPLv3 - see the LICENSE.md file for details.
--- server.py
+++ server.py
@@ -1,4 +1,23 @@
-import json
+#   gremlins-photobot is a free as in freedom server systen for collating photos with friends.
+#   Copyright (C) 2019  Edward Salkield
+#   edward.salkield@gmail.com
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+import json, configparser
 import os.path
 import os
 import sys
@@ -7,16 +26,16 @@ from werkzeug.utils import secure_filename
 import urllib.parse
 from exif import Image
 from flask import Flask, render_template, request, redirect, session, url_for
-app = Flask(__name__)
+
 
 class jsonmanager():
     users = {}
-    PHOTOPATH = ""
-    USERFILE = ""
+    PHOTO_LOCATION = ""
+    USER_FILE_LOCATION = ""
 
-    def __init__(self, PHOTOPATH, USERFILE):
+    def __init__(self, PHOTO_LOCATION, USER_FILE_LOCATION):
         # Load user accounts, create if do not exist
-        self.USERPATH = os.path.join(PHOTOPATH, USERFILE)
+        self.USERPATH = os.path.join(PHOTO_LOCATION, USER_FILE_LOCATION)
         if os.path.isfile(self.USERPATH):
             with open(self.USERPATH, "r") as f:
                 self.users = json.load(f)
@@ -25,11 +44,11 @@ class jsonmanager():
             with open(self.USERPATH, "w+") as f:
                 json.dump(self.users, f)
 
-        self.PHOTOPATH = PHOTOPATH
-        self.USERFILE = USERFILE
+        self.PHOTO_LOCATION = PHOTO_LOCATION
+        self.USER_FILE_LOCATION = USER_FILE_LOCATION
 
     def save(self):
-        with open(os.path.join(self.PHOTOPATH, self.USERPATH), "w") as f:
+        with open(self.USERPATH, "w") as f:
             json.dump(self.users, f)
 
     def reload(self):
@@ -45,9 +64,16 @@ class usermanager(jsonmanager):
         except KeyError:
             return None
 
+    def isadmin(self, username):
+        self.reload()
+        try:
+            return self.users[username]["admin"]
+        except KeyError:
+            return False
+
 
 class photomanager():
-    PHOTOPATH = ""
+    PHOTO_LOCATION = ""
     cache = {}
     metaname = "meta.json"
     cacherecord = {}
@@ -55,10 +81,10 @@ class photomanager():
     ALBUMCOLS = []
     PHOTOCOLS = []
 
-    def __init__(self, PHOTOPATH, PHOTOCOLS, ALBUMCOLS, ALLOWED_EXTENSIONS, DATE_FORMAT):
-        if not os.path.exists(PHOTOPATH):
-            os.makedirs(PHOTOPATH)
-        self.PHOTOPATH = PHOTOPATH
+    def __init__(self, PHOTO_LOCATION, PHOTOCOLS, ALBUMCOLS, ALLOWED_EXTENSIONS, DATE_FORMAT):
+        if not os.path.exists(PHOTO_LOCATION):
+            os.makedirs(PHOTO_LOCATION)
+        self.PHOTO_LOCATION = PHOTO_LOCATION
         self.ALLOWED_EXTENSIONS = ALLOWED_EXTENSIONS
         self.DATE_FORMAT = DATE_FORMAT
         self.PHOTOCOLS = PHOTOCOLS
@@ -81,12 +107,12 @@ class photomanager():
 
         if album == None:
             self.cache = {}
-            albums = os.listdir(self.PHOTOPATH)
+            albums = os.listdir(self.PHOTO_LOCATION)
         else:
             albums = [album]
 
         for albumname in albums:
-            fullpath = os.path.join(self.PHOTOPATH, albumname, self.metaname)
+            fullpath = os.path.join(self.PHOTO_LOCATION, albumname, self.metaname)
             ALBUMCOLS = []
             PHOTOCOLS = []
 
@@ -100,7 +126,7 @@ class photomanager():
     def setalbumdata(self, albumname, albumdata):
         self.cache[albumname] = albumdata
         
-        metapath = os.path.join(self.PHOTOPATH, albumname, self.metaname)
+        metapath = os.path.join(self.PHOTO_LOCATION, albumname, self.metaname)
         if os.path.exists(metapath):
             with open(metapath, 'r+') as f:
                 album = json.load(f)
@@ -137,7 +163,7 @@ class photomanager():
 
 
     def getalbumphotodata(self, albumname):
-        with open(os.path.join(self.PHOTOPATH, albumname, self.metaname), 'r') as f:
+        with open(os.path.join(self.PHOTO_LOCATION, albumname, self.metaname), 'r') as f:
             photodata = json.load(f)["photos"]
 
         photorecords = {}
@@ -153,7 +179,7 @@ class photomanager():
 
     # Create new photoalbum
     def createalbum(self, albumname, displayname, creator, date, restricted):
-        albumpath = os.path.join(self.PHOTOPATH, albumname)
+        albumpath = os.path.join(self.PHOTO_LOCATION, albumname)
         
         # Create album directory
         if os.path.exists(albumpath):
@@ -179,7 +205,7 @@ class photomanager():
         if not self.allowed_file(photoname):
             return None
 
-        photopath = os.path.join(self.PHOTOPATH, albumname, photoname)
+        photopath = os.path.join(self.PHOTO_LOCATION, albumname, photoname)
         # Test if the file exists
         if os.path.exists(photopath):
             return
@@ -197,7 +223,7 @@ class photomanager():
 
 
         # Update the album metadata
-        albumpath = os.path.join(self.PHOTOPATH, albumname, self.metaname)
+        albumpath = os.path.join(self.PHOTO_LOCATION, albumname, self.metaname)
         with open(albumpath, 'r') as f:
             metadata = json.load(f)
 
@@ -217,16 +243,49 @@ class photomanager():
 
 
 
+app = Flask(__name__)
+try:
+    app.config.from_envvar('GREMLINS_PHOTOBOT_SETTINGS')
+except Exception:
+    app.config.from_pyfile('server.conf')
 
-PHOTOPATH = "./static/photos/"
-USERFILE = "users.json"
+app.secret_key = os.urandom(24)
+
+# Update configuration
+try:
+    USER_FILE_LOCATION = app.config["USER_FILE_LOCATION"]
+except KeyError:
+    USER_FILE_LOCATION = "users.json"
+
+try:
+    ALLOWED_EXTENSIONS = set(app.config['ALLOWED_EXTENSIONS'])
+except KeyError:
+    ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+try:
+    DATE_FORMAT = app.config['DATE_FORMAT']
+except KeyError:
+    DATE_FORMAT = "%Y-%m-%d %H-%M-%S"
+
+try:
+    STATIC_LOCATION = app.config['STATIC_LOCATION']
+    app.static_folder=STATIC_LOCATION
+except KeyError:
+    STATIC_LOCATION = "./static/"
+
+try:
+    PHOTO_RELATIVE_LOCATION = app.config['PHOTO_LOCATION']
+    PHOTO_LOCATION = STATIC_LOCATION + PHOTO_RELATIVE_LOCATION
+except KeyError:
+    PHOTO_LOCATION = STATIC_LOCATION + "photos/"
+
+
+
 ALBUMCOLS = ["Display Name", "Creator", "Date", "Number of Photos", "Last Modified", "Restricted"]
 PHOTOCOLS = ["Display Name", "Creator", "Date", "Last Modified"]
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-DATE_FORMAT = "%Y-%m-%d %H-%M-%S"
 
-userman = usermanager('', USERFILE)
-photoman = photomanager(PHOTOPATH, PHOTOCOLS, ALBUMCOLS, ALLOWED_EXTENSIONS, DATE_FORMAT)
+userman = usermanager('', USER_FILE_LOCATION)
+photoman = photomanager(PHOTO_LOCATION, PHOTOCOLS, ALBUMCOLS, ALLOWED_EXTENSIONS, DATE_FORMAT)
 
 
 def authenticate(request):
@@ -281,7 +340,7 @@ def index():
         #except KeyError:
         #    return "Malformatted form."
 
-        albumname = urllib.parse.quote(displayname.lower().replace(" ", "_"))
+        albumname = urllib.parse.quote(displayname.lower().replace(" ", "_").replace("/", ""))
 
         try:
             photoman.createalbum(albumname, displayname, name, date, restricted)
@@ -307,11 +366,13 @@ def albumpage(albumname):
     # Display the album page
     if request.method == 'GET':
         if not albumname in photoman.cache.keys():
-            return "404 - album not found"
+            return albumname + "\n" + str(photoman.cache)
+            return render_template('404.html'), 404
+
         
         metadata = zip(ALBUMCOLS, photoman.cacherecord[albumname])
         photodata = photoman.sortby(photoman.getalbumphotodata(albumname), "Date", True, PHOTOCOLS)
-        return render_template("album.html", albumname=albumname, metadata=metadata, cols=PHOTOCOLS, photodata=photodata)
+        return render_template("album.html", albumname=albumname, metadata=metadata, cols=PHOTOCOLS, photodata=photodata, photolocation = "/static/" + PHOTO_RELATIVE_LOCATION)
 
     # Allow file uploads
     if request.method == 'POST':
@@ -335,13 +396,54 @@ def albumpage(albumname):
 
         return redirect(url_for('albumpage', albumname=albumname))
 
+
+@app.route("/admin", methods=['GET', 'POST'])
+def adminpage():
+    try:
+        name = session["name"]
+    except KeyError:
+        return render_template('404.html'), 404
+
+
+    if request.method == 'GET':
+        if not is_authenticated(session) or not userman.isadmin(name):
+            return render_template('404.html'), 404
+
+        return render_template("admin.html")
+    
+    elif request.method == 'POST':
+        if not is_authenticated(session) or not userman.isadmin(name):
+            return render_template('404.html'), 404
+
+        try:
+            reindex = request.form["reindex"]
+        except KeyError:
+            reindex = None
+
+        if reindex != None:
+            photoman.reindex()
+            return "reindex successful."
+
+        try:
+            reindexusers = request.form["reindexusers"]
+        except KeyError:
+            reindexusers = None
+
+        if reindexusers != None:
+            userman.reload()
+            return "reindexusers successful."
+
+        return redirect(url_for('adminpage'))
+
+
 # 404 page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-app.secret_key = os.urandom(24)
-
 
 if __name__ == '__main__':
-    app.run(port=8080, host=0.0.0.0)
+    # Configure flask
+
+    
+    app.run()
